4c16c2619e95a7e19477baf2a84ca1ccd014b012
Matt Boutell <boutell@gmail.com>
2015-04-22 11:05:31 -0400

    Hardcoded angular pic array

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,15 @@
 (function() {
-  var app = angular.module('weatherpicsApp', []);
+  var app = angular.module("weatherpicsApp", []);
   
+  app.controller("WeatherpicsController", function() {
+    this.pics = pics;
+  });
   
+  var pics = [
+              {image_url: "http://severe-wx.pbworks.com/f/tornado.jpg", caption:"Wow!"},
+              {image_url: "http://upload.wikimedia.org/wikipedia/commons/6/6b/Mount_Carmel_forest_fire14.jpg", caption:"Big fire"},
+              {image_url: "http://www.duskyswondersite.com/wp-content/uploads/2011/02/weather-cloud-32.jpg", caption:"Clouds"},
+              ];
   
   
 })();

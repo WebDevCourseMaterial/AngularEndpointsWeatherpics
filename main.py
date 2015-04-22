8dede472355fbb41886ac0d94aa2e1a1094201e6
Matt Boutell <boutell@gmail.com>
2015-04-22 10:33:31 -0400

    Update to webdev version with edit/delete

--- main.py
+++ main.py
@@ -18,29 +18,49 @@ import os
 
 from google.appengine.ext import ndb
 import jinja2
+from models import Weatherpic
 import webapp2
 
-from models import Weatherpic
 
-# Jinja environment instance necessary to use Jinja templates.
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), autoescape=True)
+jinja_env = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  autoescape=True)
 
-# Generic key used to group MovieQuotes into an entity group.
+# Generic key used to group Weatherpics into an entity group.
 PARENT_KEY = ndb.Key("Entity", 'weatherpic_root')
 
-class MainHandler(webapp2.RequestHandler):
+class WeatherpicsPage(webapp2.RequestHandler):
     def get(self):
-        weatherpics = Weatherpic.query(ancestor=PARENT_KEY).order(-Weatherpic.last_touch_date_time).fetch()
+        weatherpics_query = Weatherpic.query(ancestor=PARENT_KEY).order(-Weatherpic.last_touch_date_time).fetch()
         template = jinja_env.get_template("templates/weatherpics.html")
-        self.response.out.write(template.render({'weatherpics': weatherpics}))
+        self.response.out.write(template.render({'weatherpics_query': weatherpics_query}))
 
+
+class InsertWeatherpicAction(webapp2.RequestHandler):
     def post(self):
-        new_weather_pic = Weatherpic(parent=PARENT_KEY,
-                               image_url=self.request.get('image_url'),
-                               caption=self.request.get('caption'))
-        new_weather_pic.put()
+        if self.request.get("entity_key"):
+            weatherpic_key = ndb.Key(urlsafe=self.request.get('entity_key'))
+            weatherpic = weatherpic_key.get()
+            weatherpic.image_url = self.request.get("image_url")
+            weatherpic.caption = self.request.get("caption")
+            weatherpic.put()
+        else:
+            new_weather_pic = Weatherpic(parent=PARENT_KEY,
+                                         image_url=self.request.get('image_url'),
+                                         caption=self.request.get('caption'))
+            new_weather_pic.put()
         self.redirect(self.request.referer)
 
+
+class DeleteWeatherpicAction(webapp2.RequestHandler):
+    def post(self):
+        weatherpic_key = ndb.Key(urlsafe=self.request.get('entity_key'))
+        weatherpic_key.delete()
+        self.redirect(self.request.referer)
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ("/", WeatherpicsPage),
+    ("/insertweatherpic", InsertWeatherpicAction),
+    ("/deleteweatherpic", DeleteWeatherpicAction)
 ], debug=True)
